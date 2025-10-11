f672cc25795b4af7286fd018ea082f1754fea2cb
sai goud <saikumarmogulla40@gmail.com>
2025-10-11 19:15:04 +0530

    variables

--- 12-installation.sh
+++ 12-installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash/
 
-USERID=$(id-u)
+USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     eco "ERROR:: please run this script with root privelege"
@@ -11,4 +11,5 @@ if [ $? -ne 0 ]; then
     echo "ERROR:: Installing MYSQL is failure"
 else
     echo "Installing MYSQL is SUCCESS"
+    
 fi            
